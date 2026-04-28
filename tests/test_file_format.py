5cdf5e5cecdc53edfe1dc07ff48fb5d000a851a9
VladiusVostokus <kovalenkov222@gmail.com>
2026-04-28 18:34:39 +0300

    Add more tests for get file format

--- tests/test_file_format.py
+++ tests/test_file_format.py
@@ -5,5 +5,12 @@ class TestSimpleGetFormat(unittest.TestCase):
     def test_pdf(self):
         self.assertEqual(get_file_format('somework.pdf'), 'pdf')
 
+    def test_docx(self):
+        self.assertEqual(get_file_format('somework.docx'), 'docx')
+
+    def test_jpeg(self):
+        self.assertEqual(get_file_format('somework.jpeg'), 'jpeg')
+
+
 if __name__ == '__main__':
     unittest.main()
