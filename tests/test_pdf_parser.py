ab0a0e7168f9163ff994d93dd5e17e94be7ffe39
VladiusVostokus <kovalenkov222@gmail.com>
2026-04-29 18:34:12 +0300

    Add commented test for parsing of full file
    This test is failing now and tests how parser can get text from
    pdf with few pages. Implementation of parser isn't finished yet
    but test is needed here to show how it should works. Test is co
    mmented co github action doesn't fail

--- tests/test_pdf_parser.py
+++ tests/test_pdf_parser.py
@@ -1,5 +1,6 @@
 import unittest
 from work_file_parsers.pdf_parser import PdfParser
+from files.parsed_file2 import work_file
 
 class TestPdfParser(unittest.TestCase):
     def test_simple_pdf(self):
@@ -7,3 +8,12 @@ class TestPdfParser(unittest.TestCase):
         parser = PdfParser(file)
         work_data = parser.get_all_content()
         self.assertEqual(work_data, 'Some file with text \n')
+
+    #def test_work_pds(self):
+        #file = 'tests/files/file2.pdf'
+        #parser = PdfParser(file)
+        #work_data = parser.get_all_content()
+        #self.assertEqual(work_data, work_file)
+        
+if __name__ == '__main__':
+    unittest.main()
