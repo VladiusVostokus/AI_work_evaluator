6064081b1a2977228441f0f09c70fea40bbabc92
VladiusVostokus <kovalenkov222@gmail.com>
2026-04-29 11:34:42 +0300

    Add folder with file parsers

---
+++ work_file_parsers/pdf_parser.py
@@ -0,0 +1,12 @@
+from interfaces.work_parser import WorkParser
+
+class PdfParser(WorkParser):
+    def __init__(self):
+        pass
+
+    def get_all_content():
+        pass
+
+    def get_all_tables():
+        pass
+    
