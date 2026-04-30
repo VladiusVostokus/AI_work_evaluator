a77ae21d801dcfb724f5dad8c1ccca5f0d1d9693
VladiusVostokus <kovalenkov222@gmail.com>
2026-04-30 16:22:36 +0300

    Create interface for llm api's

---
+++ interfaces/llm_model.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class LLMModel(ABC):
+    
+    @abstractmethod
+    def form_message():
+        pass
+
+    @abstractmethod
+    def make_request():
+        pass
+    
+    @abstractmethod
+    def get_resonse():
+        pass
