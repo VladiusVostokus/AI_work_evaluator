82d94a1e977953e9c3b342bb734ae9f80ec7ba53
VladiusVostokus <kovalenkov222@gmail.com>
2026-05-01 20:35:46 +0300

    Select file with criterias for llm prompt

--- api/gemini.py
+++ api/gemini.py
@@ -2,7 +2,7 @@ from google import genai
 from interfaces.llm_model import LLMModel
 import os
 from dotenv import load_dotenv
-from message_template_parts.sys_msg import sys_msg_template, role_descriprion, response_format, response_constrains, task_description, task_structure, criteria_description, criteria_format
+from message_template_parts.sys_msg import sys_msg_template, role_descriprion, response_format, response_constrains, task_description, task_structure, criteria_format
 from work_file_parsers.parser_factory import work_parser
 
 
@@ -13,13 +13,15 @@ class Gemini(LLMModel):
         self.client = genai.Client(api_key=GEM_API)
         self.model = model_name
 
-    def form_message(self, file):
+    def form_message(self, file, criteria):
+        criteria_parser = work_parser(criteria)
+        criteria = criteria_parser.get_all_content()
         self.sys_msg = sys_msg_template.to_string(
             role_descriprion=role_descriprion,
             task_description=task_description,
             task_structure=task_structure,
             criteria_format=criteria_format,
-            criteria_description=criteria_description,
+            criteria_description=criteria,
             response_constrains=response_constrains,
             response_format=response_format)
         
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from api.llm_api_factory import llm_api
 print("Введіть ім'я роботи для перевірки:")
 file = input()
 
+print("Введіть ім'я файлу з критеріями оцінювання:")
+criteria = input()
+
 print("Введіть ім'я мовної моделі, яку хочете використати:")
 llm_name = input()
 llm = llm_api(llm_name)
 if llm != None:
-    llm.form_message(file)
+    llm.form_message(file, criteria)
     llm.make_request()
     print(llm.get_response())
 else:
