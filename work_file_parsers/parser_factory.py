85cca6f31aa886fba10156f2e81fb1e2428422e6
VladiusVostokus <kovalenkov222@gmail.com>
2026-05-05 17:35:35 +0300

    Add docx parser to parser factory

--- work_file_parsers/parser_factory.py
+++ work_file_parsers/parser_factory.py
@@ -1,8 +1,10 @@
 from work_file_parsers.pdf_parser import PdfParser
+from work_file_parsers.docx_parser import DocxParser
 from utils.get_file_format import get_file_format
 
 parsers = {
     'pdf': PdfParser,
+    'docx': DocxParser,
 }
 
 def work_parser(file_name):
