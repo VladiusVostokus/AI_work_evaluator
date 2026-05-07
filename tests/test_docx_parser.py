6f846bf8986237211f0f20942fa007dcd4e73f1e
VladiusVostokus <kovalenkov222@gmail.com>
2026-05-07 18:16:13 +0300

    Implement Docx parser and cover it with tests

--- tests/test_docx_parser.py
+++ tests/test_docx_parser.py
@@ -1,12 +1,42 @@
 import unittest
 from work_file_parsers.docx_parser import DocxParser
 
+
 class TestDocxParser(unittest.TestCase):
     def test_simple_docx(self):
         file = 'tests/files/file1.docx'
         parser = DocxParser(file)
         work_data = parser.get_all_content()
-        self.assertEqual(work_data, 'Some file with text')
+        self.assertEqual(len(work_data), 1)
+        self.assertEqual(work_data[0]['type'], 'text')
+        self.assertEqual(work_data[0]['data'], 'Some file with text')
+    
+    def test_table_docx(self):
+        file = 'tests/files/doc.docx'
+        parser = DocxParser(file)
+        work_data = parser.get_all_content()
+        self.assertEqual(len(work_data), 1)
+        self.assertEqual(work_data[0]['type'], 'table')
+
+    def test_complex_docx(self):
+        file = 'tests/files/doc2.docx'
+        parser = DocxParser(file)
+        work_data = parser.get_all_content()
+        docx_block_count = 7
+        self.assertEqual(len(work_data), docx_block_count)
+        type_list = ['text', 'table', 'text', 'text', 'table', 'table', 'text']
+        for i, block in enumerate(work_data):
+            self.assertEqual(block['type'], type_list[i])
+
+    def test_parsed_complex_docx(self):
+        file = 'tests/files/doc3.docx'
+        parser = DocxParser(file)
+        work_data = parser.get_all_content()
+        docx_block_count = 3
+        self.assertEqual(len(work_data), docx_block_count)
+        parsed_data = parser.get_parsed_data()
+        result_table = 'Some text\n\n===TABLE START===\n| 1 | 2 | 3 |\n| --- | --- | --- |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |\n===TABLE END===\n\nSome text 2\n\n'
+        self.assertEqual(parsed_data, result_table)
 
 if __name__ == '__main__':
     unittest.main()
--- work_file_parsers/docx_parser.py
+++ work_file_parsers/docx_parser.py
@@ -1,15 +1,42 @@
 from interfaces.work_parser import WorkParser
 from docx import Document
+from docx.document import Document as DOC_TYPE
+from docx.text.paragraph import Paragraph
+from docx.table import Table
+from docx.oxml.text.paragraph import CT_P
+from docx.oxml.table import CT_Tbl
+from utils.mark_table_bounds import mark_table_bounds
 
 class DocxParser(WorkParser):
     def __init__(self, filename):
         self.file = filename
 
+    def __generate_doc_objects(self, doc):
+        if isinstance(doc, DOC_TYPE):
+            parent_el = doc.element.body
+        else:
+            parent_el = doc._element
+
+        for child in parent_el.iterchildren():
+            if isinstance(child, CT_P):
+                yield Paragraph(child, doc)
+            elif isinstance(child, CT_Tbl):
+                yield Table(child, doc)
+
     def get_all_content(self):
         document = Document(self.file)
-        result = ''
-        for paragraph in document.paragraphs:
-            result += paragraph.text
+        result = []
+        for item in self.__generate_doc_objects(document):
+            if isinstance(item, Paragraph):
+                text = item.text.strip()
+                if text:
+                    result.append({'type':'text', 'data': text})
+            elif isinstance(item, Table):
+                table_data = []
+                for row in item.rows:
+                    row_content = [cell.text.strip() for cell in row.cells]
+                    table_data.append(row_content)
+                result.append({'type':'table', 'data': table_data})
         return result
 
 
@@ -17,4 +44,6 @@ class DocxParser(WorkParser):
         pass
 
     def get_parsed_data(self):
-        pass
+        file_content = self.get_all_content()
+        parsed_data = mark_table_bounds(file_content)
+        return parsed_data
