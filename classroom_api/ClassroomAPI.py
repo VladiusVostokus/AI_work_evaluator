50b24f9ed052ef68e89bc743251e830914ed8e21
VladiusVostokus <kovalenkov222@gmail.com>
2026-05-27 20:01:04 +0300

    build servise

--- classroom_api/ClassroomAPI.py
+++ classroom_api/ClassroomAPI.py
@@ -8,16 +8,29 @@ from googleapiclient.errors import HttpError
 
 class ClassroomAPI:
     def __init__(self, SCOPES: list, credentials: str, token_json: str):
+        self.creds: Credentials
+        self.servise = {}
         if os.path.exists(token_json):
             creds = Credentials.from_authorized_user_file(token_json, SCOPES)
+            self.creds = creds
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
+                self.creds = creds
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     credentials, SCOPES
                 )
                 creds = flow.run_local_server(port=0)
+                self.creds = creds
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
+
+    def build_servise(self, servise_name: str, version: str):
+        servise = build(servise_name, version, credentials=self.creds)
+        self.servise[servise_name] = servise
+
+    def get_servises(self):
+        return self.servise
+    
 
