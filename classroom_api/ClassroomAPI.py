771fd001d0f3dfd08845ccb16c6f8109c8c0ae9a
VladiusVostokus <kovalenkov222@gmail.com>
2026-05-27 19:35:05 +0300

    Fix credentials variable

--- classroom_api/ClassroomAPI.py
+++ classroom_api/ClassroomAPI.py
@@ -15,7 +15,7 @@ class ClassroomAPI:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    "credentials.json", SCOPES
+                    credentials, SCOPES
                 )
                 creds = flow.run_local_server(port=0)
             with open("token.json", "w") as token:
