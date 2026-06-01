28f4650d9f4f9135d56fd3b0425b920c8c041bd1
VladiusVostokus <kovalenkov222@gmail.com>
2026-06-01 19:24:23 +0300

    Add parameters to llm interface

--- interfaces/llm_model.py
+++ interfaces/llm_model.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from store_api.task_dto import Task
 
 class LLMModel(ABC):
     
     @abstractmethod
-    def form_message():
+    def form_message(subject_name: str, task_path: str, task_data: Task):
         pass
 
     @abstractmethod
